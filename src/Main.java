ebd77121bf2cd5c2f2fc35f23c067bc3816bbf36
Facundo Preiss <115724589+facupreiss@users.noreply.github.com>
2022-10-24 12:35:40 -0300

    feat: complete teams

--- src/Main.java
+++ src/Main.java
@@ -14,15 +14,33 @@ public class Main {
         torneoDaVinci.setStartDate(comienzoDeCursada);
         torneoDaVinci.setEndDate(finDeCursada);
 
-        Player profeSergio = new Player("Sergio Medina", "Goalkeeper");
+        Player profeSergio = new Player("Sergio Medina", "Arquero");
         Player profeSuplente = new Player("Suplente", "Defender");
+        Player profeBase = new Player ("Victor Golemba", "Delantero");
+        Player profePert = new Player ("Lautaro Cciapero", "Mediocampista");
+        Player profeWeb = new Player ("Juan Carloni", "Lateral");
+
+        Player estFacuPreiss = new Player ("Facundo Preiss", "Mediocampista");
+        Player estKevin = new Player ("Daniel Kevin Colina", "Defensor");
+        Player estFacuNeiman = new Player ("Facundo Ezequiel Neiman", "Delantero");
+        Player estLucasEmens = new Player ("Lucas Patricio Emens", "Defensa");
+        Player estSantiFazio = new Player ("Santino Tomás Fazio", "Arquero");
 
 
         Team equipoPoo = new Team("Profes POO");
         equipoPoo.addPlayer(profeSergio);
         equipoPoo.addPlayer(profeSuplente);
+        equipoPoo.addPlayer(profeBase);
+        equipoPoo.addPlayer(profePert);
+        equipoPoo.addPlayer(profeWeb);
 
         Team estudiantesPoo = new Team("Estudiantes POO");
+        estudiantesPoo.addPlayer(estFacuPreiss);
+        estudiantesPoo.addPlayer(estKevin);
+        estudiantesPoo.addPlayer(estFacuNeiman);
+        estudiantesPoo.addPlayer(estLucasEmens);
+        estudiantesPoo.addPlayer(estSantiFazio);
+
 
         Match primerParcial = new Match(fechaParcial,"Aula 116", equipoPoo, estudiantesPoo );
 
